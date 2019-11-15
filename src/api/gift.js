af02cba338928f3f26b15c5f9d83c0cae4b9d5a4
lijieru918 <1353654876@qq.com>
2019-11-15 17:07:52 +0800

    礼品添加初次代码

--- src/api/gift.js
+++ src/api/gift.js
@@ -3,7 +3,19 @@ import AXIOS from '@/libs/request'
 export function viewGifts(){
     return AXIOS.get('/product/product/')
 }
+//查看礼品分类
+export function viewGiftsClass(){
+  return AXIOS.get('/product/category/')
+}
+//查看礼品规格
+export function viewGiftsSpecs(){
+  return AXIOS.get('/product/specifications/')
+}
 //上传附件
 export function uploadAttachment(params) {
     return AXIOS.post('/appfile/appfile/', params)
   }
+  //添加礼品
+  export function addGifts(params) {
+    return AXIOS.post('/product/product/', params)
+  }  
--- src/views/gift-manage/index.vue
+++ src/views/gift-manage/index.vue
@@ -1,38 +1,114 @@
 <template>
-  <el-row :gutter="30">
-    <el-col :span="6" v-for="(item,index) in giftList" :key="index">
-      <el-card shadow="hover" :body-style="{ padding: '0px' }">
-        <div class=".el-card__header">
-          <div slot="header" class="box-card-header">
-            <img src="../../icons/bkground.jpg" class="image" />
+  <div>
+    <el-row>
+      <el-col :span="24" style="text-align:right">
+        <el-button type="primary" :rules="rules" @click="openDialog(2)" icon="el-icon-plus">礼品添加</el-button>
+      </el-col>
+    </el-row>
+    <el-row :gutter="30">
+      <el-col :span="6" v-for="(item,index) in giftList" :key="index">
+        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+          <div class=".el-card__header">
+            <div slot="header" class="box-card-header">
+              <el-image
+                class="mini-image"
+                :src="item.picture?$store.state.BASE_URL+item.picture:''"
+                fit="cover"
+                style=" width: 100%;
+                              height: auto;
+"
+              >
+                <div slot="error" class="image-slot">
+                  <i class="el-icon-picture" style="color:#909399"></i>
+                </div>
+              </el-image>
+            </div>
+            <!-- <div slot="header" class="box-card-header">
+              <img src="../../icons/bkground.jpg" class="image" />
+            </div>-->
           </div>
+          <div style="padding: 18px;">
+            <p>创建人：{{item.creator_info.username}}</p>
+            <el-row>
+              <el-col :span="8">
+                <p class="subtitle">分类</p>
+                <div>{{item.category}}</div>
+              </el-col>
+              <el-col :span="14">
+                <p class="subtitle">名称</p>
+                <div style="display:flex;">
+                  <div>{{item.specifications.name}}</div>
+                </div>
+              </el-col>
+              <el-col :span="8">
+                <p class="subtitle">数量</p>
+                <div>{{item.specifications.num}}</div>
+              </el-col>
+              <el-col :span="15">
+                <p class="subtitle">标题</p>
+                <div>{{item.title}}</div>
+              </el-col>
+            </el-row>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-dialog title="礼品添加" :visible.sync="dialogShow2" width="400px">
+      <el-form :model="addGiftsForm" status-icon ref="addGiftsForm" label-width="90px">
+        <el-upload
+          accept="picture/jpeg, picture/gif, picture/png"
+          ref="upload"
+          class="upload-demo"
+          action="/api/appfile/appfile/"
+          :headers="headers"
+          :on-success="handleSuccess"
+          drag
+          :show-file-list="false"
+        >
+          <el-image v-if="SRC" style="width: 100%; height: 100%" :src="SRC"></el-image>
+          <template v-else>
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">
+              将文件拖到此处，或
+              <em>点击上传</em>
+            </div>
+          </template>
+        </el-upload>
+        <!-- <el-form-item label="创建人" prop="creator">
+          <el-input v-model="addGiftsForm.creator"></el-input>
+        </el-form-item>-->
+        <el-form-item label="标题" prop="title">
+          <el-input v-model="addGiftsForm.title"></el-input>
+        </el-form-item>
+        <el-form-item label="礼品说明" prop="content">
+          <el-input v-model="addGiftsForm.content"></el-input>
+        </el-form-item>
+        <el-form-item label="分类" prop="category">
+          <!-- <el-input  v-model="addGiftsForm.category"></el-input> -->
+          <el-select v-model="addGiftsForm.category" filterable placeholder="请选择分类">
+            <el-option v-for="item of giftsClassList" :label="item" :value="item" :key="item"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="名称" prop="spec_name">
+          <el-input v-model="addGiftsForm.spec_name"></el-input>
+        </el-form-item>
+        <el-form-item label="数量" prop="spec_number">
+          <el-input v-model.number="addGiftsForm.spec_number"></el-input>
+        </el-form-item>
+        <el-form-item label="规格说明" prop="spec_content">
+          <el-input v-model.number="addGiftsForm.spec_content"></el-input>
+        </el-form-item>
+        <el-form-item label="价格" prop="spec_price">
+          <el-input v-model.number="addGiftsForm.spec_price"></el-input>
+        </el-form-item>
+
+        <div style="text-align:right">
+         <el-button @click="cancel">取消</el-button>
+          <el-button type="primary" @click="submitForm('addGiftsForm')">提交</el-button>
         </div>
-        <div style="padding: 18px;">
-          <p>创建人：{{item.creator_info.username}}</p>
-          <el-row>
-            <el-col :span="8">
-              <p class="subtitle">分类</p>
-              <div>{{item.category}}</div>
-            </el-col>
-            <el-col :span="14">
-              <p class="subtitle">名称</p>
-              <div style="display:flex;">
-                <div>{{item.specifications.name}}</div>
-              </div>
-            </el-col>
-            <el-col :span="8">
-              <p class="subtitle">数量</p>
-              <div>{{item.specifications.num}}</div>
-            </el-col>
-            <el-col :span="15">
-              <p class="subtitle">标题</p>
-              <div>{{item.title}}</div>
-            </el-col>
-          </el-row>
-        </div>
-      </el-card>
-    </el-col>
-  </el-row>
+      </el-form>
+    </el-dialog>
+  </div>
   <!-- <div class="block" style="text-align: right;margin-top:10px">
       <el-pagination
         @size-change="handleSizeChange"
@@ -47,12 +123,43 @@
 </template>
 
 <script>
-import { viewGifts } from "@/api/gift";
+import {
+  viewGifts,
+  addGifts,
+  viewGiftsClass,
+  viewGiftsSpecs
+} from "@/api/gift";
+import { getToken } from "@/utils/auth";
 export default {
   data() {
     return {
-      giftList: []
+      giftList: [],
+      giftsClassList: [],
+      dialogShow2: false,
+      addGiftsForm: {
+        //  creator: null,
+        category: null,
+        spec_name: null,
+        title: null,
+        picture: null,
+        spec_price: null,
+        spec_content: null,
+        content: null
+      },
 
+      rules: {
+        spec_number: [
+          {
+            pattern: /^[+]{0,1}(\d+)$|^[+]{0,1}(\d+\.\d+)$/,
+            message: "请输入数字",
+            trigger: "change"
+          }
+        ]
+      },
+      SRC: "",
+      headers: {
+        Authorization: `JWT ${getToken()}`
+      }
       //  currentPage: 1,
       // pageSize: 20,
       // pageSizeList: [10, 20, 50, 100]
@@ -63,11 +170,54 @@ export default {
     getviewGifts() {
       viewGifts().then(({ data }) => {
         this.giftList = [...data.msg];
-        this.currentPage = 1;
-        console.log("1111111");
-        console.log(this.giftList);
       });
-    }
+    },
+    getGiftsClass() {
+      viewGiftsClass().then(({ data }) => {
+        this.giftsClassList = [...data.msg];
+      });
+    },
+
+    openDialog(Type) {
+      if (Type === 2) {
+        this.dialogShow2 = true;
+      }
+    },
+    // resetForm(formName) {
+    //   this.$refs[formName].resetFields();
+    // },
+    //监听上传图片成功，成功后赋值给form ，并且赋值给图片src显示图片
+    handleSuccess(response, file, fileList) {
+      if (response.status == 0) {
+        this.SRC = this.$store.state.BASE_URL + response.msg;
+        this.addGiftsForm["picture"] = response.msg;
+        // this.addGiftsForm.image_id = response.id;
+      } else {
+        this.$message.error(response.msg);
+      }
+    },
+    //添加礼品
+    submitForm(addGiftsForm) {
+      console.log("111111");
+      console.log(this.addGiftsForm);
+      addGifts(this.addGiftsForm).then(({ data }) => {
+        if (data.status === 0) {
+          this.$message.success(data.msg);
+          this.dialogShow2 = false;
+          this.addGiftsForm = {};
+          this.getviewGifts();
+        } else {
+          this.$message.error(data.msg);
+          this.addGiftsForm = {};
+          this.getviewGifts();
+        }
+      });
+    },
+    cancel() {
+        //this.dialogShow2 = false;
+      this.addGiftsForm = [];
+      this.SRC="";
+    },
     //   // 分页
     // handleSizeChange(val) {
     //   this.pageSize = val;
@@ -85,6 +235,7 @@ export default {
 
   created() {
     this.getviewGifts();
+    this.getGiftsClass();
   }
 };
 </script>
@@ -93,7 +244,6 @@ export default {
   position: relative;
   margin: 0 5px 30px 5px;
   width: 300px;
-
 }
 .box-card-header {
   position: relative;
@@ -101,7 +251,7 @@ export default {
   overflow: hidden;
 }
 .el-card__header {
-  padding:0px 0px;
+  padding: 0px 0px;
 }
 
 p {
@@ -112,8 +262,18 @@ p {
 .subtitle {
   margin: 15px 0 5px;
 }
-.image {
-  width: 100%;
-  display: block;
-}
+ .mini-image {
+                cursor: pointer;
+                overflow: hidden;
+                opacity: 1;
+               
+            }
+            .image-slot {
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-size: 56px;
+                background: #DCDFE6;
+            }
 </style>
