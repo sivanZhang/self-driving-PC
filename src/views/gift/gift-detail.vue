4245dfe024e4dff236c302c400b83085e1a4ef22
lijieru918 <1353654876@qq.com>
2019-12-02 22:23:31 +0800

    礼品详情页的规格添加

--- src/views/gift/gift-detail.vue
+++ src/views/gift/gift-detail.vue
@@ -1,19 +1,19 @@
+<!--礼品详情-->
 <template>
   <div>
     <el-row :gutter="20">
       <el-col :span="10">
         <el-carousel :interval="5000" arrow="always">
           <el-carousel-item v-for="(item,index) in giftList" :key="index">
-            <el-image
-              class="mini-image"
-              :src="item?$store.state.BASE_URL+item:''"
-              fit="cover"
-               ></el-image>
+            <el-image class="mini-image" :src="item?$store.state.BASE_URL+item:''" fit="cover"></el-image>
           </el-carousel-item>
         </el-carousel>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="6">
         <label>标题: {{giftList2}}</label>
+      </el-col>
+      <el-col :span="8">
+        <el-button type="primary" icon="el-icon-plus" @click="openDialog(1)">添加规格</el-button>
       </el-col>
       <el-col :span="14">
         <el-table :data="giftSpeList" border style="margin-top:5px">
@@ -35,25 +35,78 @@
         </el-col>
       </el-card>
     </el-col>
+    <!--添加规格-->
+    <el-dialog title="添加规格" :visible.sync="dialogShow1" width="480px" top="5vh">
+      <el-form :model="addSpecsForm" label-width="90px" :rules="rules">
+        <el-form-item label="名称" prop="name">
+          <el-input v-model="addSpecsForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="数量" prop="number">
+          <el-input v-model.number="addSpecsForm.number"></el-input>
+        </el-form-item>
+        <el-form-item label="单价" prop="price">
+          <el-input v-model.number="addSpecsForm.price"></el-input>
+        </el-form-item>
+        <el-form-item label="虚拟币单价" prop="coin">
+          <el-input v-model.number="addSpecsForm.coin"></el-input>
+        </el-form-item>
+        <el-form-item label="规格说明" prop="content">
+          <el-input v-model="addSpecsForm.content"></el-input>
+        </el-form-item>
+        <div class="btn">
+          <el-button @click="cancel">取消</el-button>
+          <el-button type="primary" @click="submitForm('addSpecsForm')">创建</el-button>
+        </div>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { viewGifts, viewGiftsSpecs } from "@/api/gift";
+import { viewGifts, viewGiftsSpecs, addGiftSpecs } from "@/api/gift";
 import { getToken } from "@/utils/auth";
 export default {
   name: "gift-detail",
 
   data() {
     return {
+      dialogShow1: false,
       giftList: [],
       giftList1: [],
       giftList2: [],
       giftSpeList: [],
-      id: this.$route.params.id
+      id: this.$route.params.id,
+      addSpecsForm: {
+        price: null,
+        coin: null,
+        name: null,
+        content: null,
+        number: null
+      },
+      rules: {
+        name: [{ required: true, message: "名称不能为空" }],
+        number: [
+          { required: true, message: "数量不能为空" },
+          { type: "number", message: "数量必须为数字值" }
+        ],
+        price: [
+          { required: true, message: "单价不能为空" },
+          { type: "number", message: "数量必须为数字值" }
+        ],
+        coin: [
+          { required: true, message: "虚拟币不能为空" },
+          { type: "number", message: "数量必须为数字值" }
+        ]
+      }
     };
   },
   methods: {
+    openDialog(Type) {
+      if (Type === 1) {
+        this.dialogName = "添加规格";
+        this.dialogShow1 = true;
+      }
+    },
     //轮播图
     getviewGifts() {
       var id = this.id;
@@ -76,6 +129,37 @@ export default {
       viewGiftsSpecs({ product_id: id }).then(({ data }) => {
         this.giftSpeList = [...data.msg];
       });
+    },
+    //添加规格
+    submitForm(addSpecsForm) {
+      let specs = {};
+      specs = {
+        product: this.id,
+        name: this.addSpecsForm.name,
+        number: this.addSpecsForm.number,
+        content: this.addSpecsForm.content,
+        coin: this.addSpecsForm.coin,
+        price: this.addSpecsForm.price
+      };
+
+      if (specs.content == null) {
+        delete specs.content;
+      }
+      addGiftSpecs(specs).then(({ data }) => {
+        if (data.status === 0) {
+          this.$message.success(data.msg);
+          this.addSpecsForm = {};
+          this.getGiftsSpecs();
+          this.dialogShow1 = false;
+        } else {
+          this.$message.error(data.msg);
+          this.addSpecsForm = {};
+          this.getGiftsSpecs();
+        }
+      });
+    },
+    cancel() {
+      this.addSpecsForm = [];
     }
   },
   created() {
@@ -90,5 +174,8 @@ export default {
 .mini-image {
   width: 100%;
   height: auto;
+}
+.btn {
+  text-align: right;
 }
 </style>
