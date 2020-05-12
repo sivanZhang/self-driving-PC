f200326b6e553dc5a0855b0dcd3eb1a91a103c18
sunshine8866 <2837037335@qq.com>
2020-05-12 17:46:33 +0800

    添加分类管理API

---
+++ src/api/classManage.js
@@ -0,0 +1,21 @@
+import AXIOS from '@/libs/request'
+//类别详情查询
+export function getClassDetails(params){
+    return AXIOS.get('/categories/categories/?id=6',{params})
+}
+//类别查询
+export function classQuery(params){
+    return AXIOS.get('/categories/categories/',{params})
+}
+//添加类别
+export function addClass(data){
+    return AXIOS.post('/categories/categories/',data)
+}
+//删除活动
+export function delClass(data){
+    return AXIOS.post('/categories/categories/?method=delete',data)
+}
+//编辑活动
+export function editClass(data){
+    return AXIOS.post('/categories/categories/?method=put',data)
+}
